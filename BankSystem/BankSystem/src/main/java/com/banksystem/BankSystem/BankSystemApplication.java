05e233d4282281c20fae23e9a7545a174568c511
antwanhelou <120364950+antwanhelou@users.noreply.github.com>
2024-02-20 10:47:04 +0200

    Project start

---
+++ BankSystem/BankSystem/src/main/java/com/banksystem/BankSystem/BankSystemApplication.java
@@ -0,0 +1,13 @@
+package com.banksystem.BankSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemApplication.class, args);
+	}
+
+}
